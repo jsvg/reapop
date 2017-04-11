7691d8e8ff7fa4591882e8bc6304759777adb9a1
Julian V <julian.vangiessen@gmail.com>
2017-04-11 00:32:48 -0400

    seamless-immutable friendly operation

--- src/components/NotificationsContainer.js
+++ src/components/NotificationsContainer.js
@@ -45,7 +45,7 @@ export class NotificationsContainer extends Component {
     // when notifications are displayed at the bottom,
     // we display notifications from bottom to top
     if (position.startsWith('b')) {
-      notifications = notifications.slice().reverse();
+      notifications = [...notifications].reverse();
     }
 
     return notifications.map((notification) => (
